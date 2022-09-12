3b15c4860a1d6d1ce2104381f5e6776bc2a5351e
JianChoi-Kor <wldks79@gmail.com>
2022-09-12 23:05:52 +0900

    side_toon

---
+++ toon_admin/src/main/java/com/side/toon/ToonAdminApplication.java
@@ -0,0 +1,12 @@
+package com.side.toon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToonAdminApplication.class, args);
+    }
+}
---
+++ toon_main/src/main/java/com/side/toon/ToonMainApplication.java
@@ -0,0 +1,12 @@
+package com.side.toon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToonMainApplication.class, args);
+    }
+}
