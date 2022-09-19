d5f4e43bd908c01435f066263c494bc514542c04
JianChoi-Kor <wldks79@gmail.com>
2022-09-19 22:56:02 +0900

    enum

--- toon_main/src/main/java/com/side/toon/v1/domain/user/enums/UserState.java
+++ toon_main/src/main/java/com/side/toon/v1/domain/user/enums/UserState.java
@@ -1,4 +1,24 @@
 package com.side.toon.v1.domain.user.enums;
 
-public enum UserState {
+import com.side.toon.v1.lib.enums.LegacyCommonType;
+import lombok.Getter;
+
+@Getter
+public enum UserState implements LegacyCommonType {
+
+    NORMAL("정상", 1),
+    UNCERTIFIED("미인증", 2),
+    SUSPENSION("정지", 3),
+    WITHDRAWAL("탈퇴", 4)
+    ;
+
+    private String desc;
+    private Integer legacyCode;
+
+    UserState(String desc, Integer legacyCode) {
+        this.desc = desc;
+        this.legacyCode = legacyCode;
+    }
+
+
 }
---
+++ toon_main/src/main/java/com/side/toon/v1/lib/enums/LegacyCommonType.java
@@ -0,0 +1,7 @@
+package com.side.toon.v1.lib.enums;
+
+public interface LegacyCommonType {
+
+    String getLegacyCode();
+    String getDesc();
+}
