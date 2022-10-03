3c0437908e3fefec6eb64a94269b7378a119579a
JianChoi-Kor <wldks79@gmail.com>
2022-10-03 14:59:52 +0900

    toon_admin

---
+++ toon_admin/src/main/java/com/side/toon/domain/main/MainController.java
@@ -0,0 +1,20 @@
+package com.side.toon.domain.main;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/main")
+public class MainController {
+
+    @GetMapping("")
+    public ModelAndView main() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("page/main");
+        return modelAndView;
+    }
+}
--- toon_core/src/main/java/com/side/toon/domain/coin/CoinHistory.java
+++ toon_core/src/main/java/com/side/toon/domain/coin/CoinHistory.java
@@ -1,5 +1,6 @@
 package com.side.toon.domain.coin;
 
+import com.side.toon.domain.user.User;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
@@ -18,4 +19,19 @@ public class CoinHistory {
     @Column(name = "idx", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
+
+    /**
+     * 내역 고유 번호
+     */
+    @Column(name = "history_id", nullable = false)
+    private String historyId;
+
+    /**
+     * 사용자
+     */
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_idx", referencedColumnName = "idx")
+    private User user;
+
+
 }
